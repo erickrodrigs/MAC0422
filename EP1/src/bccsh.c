073f545997536680a4f9f7db6e5e0dc8c73476f7
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2020-09-17 17:52:33 -0300

    create mkdir, kill and ln
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
    Signed-off-by: Vinicius <viniciuspxfrota@gmail.com>

--- EP1/src/bccsh.c
+++ EP1/src/bccsh.c
@@ -4,7 +4,10 @@
 #include <unistd.h>
 #include <readline/readline.h> 
 #include <readline/history.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <signal.h>
 
 void type_prompt() {
   char *username, cwd[616];
@@ -13,22 +16,67 @@ void type_prompt() {
   printf("{%s@%s}", username, cwd);
 }
 
-void read_command(char **command) {
-  char *line = readline(" ");
-  *command = strsep(&line, " ");
-  printf("linha: %s\n", line);
-  printf("comando: %s\n", *command);
+void read_command(char **command, char **parameters) {
+  *parameters = readline(" ");
+  *command = strsep(parameters, " ");
 }
-
 
 int main (int argc, char **argv) {
   pid_t childpid;
-  char *command;
+  char *command, *parameters;
 
   while (1) {
     type_prompt();
-    read_command(&command);
-    printf("%s\n", command);
+    read_command(&command, &parameters);
+
+    if (strcmp(command, "/usr/bin/du") == 0) {
+      if (strcmp(command, "-hs .") == 0) {
+        // invoca aqui, não sei fazer ainda
+      }
+    }
+    else if (strcmp(command, "/usr/bin/traceroute") == 0) {
+      if (strcmp(command, "www.google.com.br") == 0) {
+        // invoca aqui, não sei fazer ainda
+      }
+    }
+    else if (strcmp(command, "./ep1") == 0) {
+      // invoca aqui, não sei fazer ainda
+    }
+    else if (strcmp(command, "mkdir") == 0) {
+      if (mkdir(parameters, 0777) == 0) {
+        printf("Criado diretório %s\n", parameters);
+      }
+      else {
+        printf("Deu ruim!\n");
+      }
+    }
+    else if (strcmp(command, "kill") == 0) {
+      char *flag = strsep(&parameters, " ");
+      int signal = flag[1] - '0';
+      pid_t pid = atoi(parameters);
+      
+      if (kill(pid, signal) == 0) {
+        printf("Processo %d finalizado!\n", pid);
+      }
+      else {
+        printf("Deu ruim!\n");
+      }
+    }
+    else if (strcmp(command, "ln") == 0) {
+      char *flag = strsep(&parameters, " ");
+      char *oldname = strsep(&parameters, " ");
+      char *newname = parameters;
+      
+      if (symlink(oldname, parameters) == 0) {
+        printf("Link para %s criado com sucesso!\n", oldname);
+      }
+      else {
+        printf("Deu ruim!\n");
+      }
+    }
+    else {
+      printf("Comando não suportado!\n");
+    }
 
     /*if ( (childpid = fork()) == 0) {
       printf("[Sou o processo filho]\n");
