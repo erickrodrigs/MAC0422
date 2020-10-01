2af03785ba14ca499e8333782d4cef56bc50cdae
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2020-10-01 17:41:57 -0300

    finish stderr and create input generator and tester
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
    Signed-off-by: Vinicius <viniciuspxfrota@gmail.com>

---
+++ EP1/tests/input_generator.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char **argv) {
+  FILE *output_file;
+  int size;
+  int i;
+  int t0 = 0, dt, deadline;
+
+  size = atoi(argv[2]);
+
+  srand(time(NULL));
+
+  output_file = fopen(argv[1], "w");
+
+  for (i = 0; i < size; i++) {
+    int n = 1 + rand() % 10;
+
+    if (n >= 4 && n <= 5) {
+      t0 = t0 + 1;
+    }
+    else if (n >= 6 && n <= 7) {
+      t0 = t0 + 2;
+    }
+    else if (n >= 8) {
+      t0 = t0 + rand() % 3 + 3;
+    }
+
+    dt = 1 + rand() % 10;
+    deadline = t0 + dt + rand() % 20;
+
+    fprintf(output_file, "p%d %d %d %d\n", i, t0, dt, deadline);
+  }
+
+  fclose(output_file);
+
+  return 0;
+}
